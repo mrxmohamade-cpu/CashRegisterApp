50677d5fa065d40922c49f46aff6179af81f4f2b
mrxmohamade-cpu <mrxmohamade@gmail.com>
2025-09-25 19:05:03 +0100

    fix: provide connection factory implementations

--- lib/core/db/connection/app_database_connection.dart
+++ lib/core/db/connection/app_database_connection.dart
@@ -3,4 +3,4 @@ import 'package:drift/drift.dart';
 import 'app_database_connection_io.dart'
     if (dart.library.html) 'app_database_connection_web.dart';
 
-QueryExecutor openConnection();
+QueryExecutor openConnection() => createConnection();
--- lib/core/db/connection/app_database_connection_io.dart
+++ lib/core/db/connection/app_database_connection_io.dart
@@ -5,7 +5,7 @@ import 'package:drift/native.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 
-LazyDatabase openConnection() {
+QueryExecutor createConnection() {
   return LazyDatabase(() async {
     final dir = await getApplicationDocumentsDirectory();
     final dbPath = p.join(dir.path, 'cash_register.sqlite');
